fb4b3e35691e01769d854da89f63d95c6e675068
haris fadhilah <haris@doku.com>
2020-08-03 22:58:59 +0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Container from "../components/container"
 
 export default function Home() {
-  return <div>Hello world!</div>
+
+
+  return (
+      <Container>
+      <h1> Hello world! </h1>
+      </Container>
+  )
 }
